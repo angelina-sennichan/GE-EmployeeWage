66be8bcd1c057127c2c1b0e4fcde89651cc23cdd
angelina <angelinasennichan@gmail.com>
2025-12-09 15:07:59 +0530

    Added: Attendance check

---
+++ src/Employee.java
@@ -0,0 +1,12 @@
+public class Employee {
+    int dailyAttendance;
+    public void calculateAttendance(){
+        if(Math.random()< 0.5){
+            dailyAttendance = 0;
+            System.out.println("Absent");
+        }else {
+            dailyAttendance = 1;
+            System.out.println("Present");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation.");
+        Employee e1 = new Employee();
+        e1.calculateAttendance();
     }
 }
