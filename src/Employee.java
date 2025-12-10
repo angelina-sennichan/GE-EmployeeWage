d92c4c59a7ba48c2ef9e05e9f1bf20face1a81b0
angelina <angelinasennichan@gmail.com>
2025-12-10 13:13:41 +0530

    Added: Switch Case Implementation

--- src/Employee.java
+++ src/Employee.java
@@ -6,28 +6,37 @@ public class Employee {
     int dailyWage;
 
     public void calculateAttendance() {
-        int randomCheck = (int)(Math.random() * 3);  // 0, 1, or 2
-        if(randomCheck == 0){
-            dailyAttendance = 0;
-            System.out.println("Absent");
-        } else if(randomCheck == 1){
-            dailyAttendance = 1;
-            System.out.println("Present: Part-time");
-        } else{
-            dailyAttendance = 2;
-            System.out.println("Present: Full-time");
+        int randomCheck = (int)(Math.random() * 3);  // 0, 1 or 2
+        switch (randomCheck) {
+            case 0:
+                dailyAttendance = 0;
+                System.out.println("Absent");
+                break;
+            case 1:
+                dailyAttendance = 1;
+                System.out.println("Present: Part-time");
+                break;
+            case 2:
+                dailyAttendance = 2;
+                System.out.println("Present: Full-time");
+                break;
         }
     }
 
     public void calculateDailyWage() {
-        if(dailyAttendance == 1){
-            dailyWage = wagePerHour * partTimeHour;
-            System.out.println("Daily Employee Wage: " + dailyWage);
-        } else if(dailyAttendance == 2){
-            dailyWage = wagePerHour * fullDayHour;
-            System.out.println("Daily Employee Wage: " + dailyWage);
-        } else {
-            System.out.println("No pay");
+        switch (dailyAttendance) {
+            case 1: // Part-time
+                dailyWage = wagePerHour * partTimeHour;
+                System.out.println("Daily Employee Wage: " + dailyWage);
+                break;
+            case 2: // Full-time
+                dailyWage = wagePerHour * fullDayHour;
+                System.out.println("Daily Employee Wage: " + dailyWage);
+                break;
+            case 0: // Absent
+            default:
+                System.out.println("No pay");
+                break;
         }
     }
 }
