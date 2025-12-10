479c8cf5890f3ae2a0d5c861510fb83177f3f528
angelina <angelinasennichan@gmail.com>
2025-12-10 13:24:37 +0530

    Added: Monthly Wage Calculation

--- src/Employee.java
+++ src/Employee.java
@@ -4,6 +4,8 @@ public class Employee {
     int fullDayHour = 8;
     int partTimeHour = 4;
     int dailyWage;
+    int workingDays = 20;
+    int totalWage = 0;
 
     public void calculateAttendance() {
         int randomCheck = (int)(Math.random() * 3);  // 0, 1 or 2
@@ -38,5 +40,15 @@ public class Employee {
                 System.out.println("No pay");
                 break;
         }
+    }
+
+    public void calculateMonthlyWage() {
+        for (int day = 1; day <= workingDays; day++) {
+            System.out.println("\nDay " + day + ":");
+            calculateAttendance();   // determine attendance
+            calculateDailyWage();    // calculate daily wage
+            totalWage += dailyWage;  // add to monthly total
+        }
+        System.out.println("\nTotal Wage for the Month: " + totalWage);
     }
 }
