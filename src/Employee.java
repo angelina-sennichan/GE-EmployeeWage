1dcfd8584a4f84b7dfcfbe0319b350bfd2f75516
angelina <angelinasennichan@gmail.com>
2025-12-10 13:05:58 +0530

    Added: Part-time Calculation

--- src/Employee.java
+++ src/Employee.java
@@ -2,22 +2,31 @@ public class Employee {
     int dailyAttendance;
     int wagePerHour = 20;
     int fullDayHour = 8;
+    int partTimeHour = 4;
     int dailyWage;
 
-    public void calculateAttendance(){
-        if(Math.random()< 0.5){
+    public void calculateAttendance() {
+        int randomCheck = (int)(Math.random() * 3);  // 0, 1, or 2
+        if(randomCheck == 0){
             dailyAttendance = 0;
             System.out.println("Absent");
-        }else {
+        } else if(randomCheck == 1){
             dailyAttendance = 1;
-            System.out.println("Present");
+            System.out.println("Present: Part-time");
+        } else{
+            dailyAttendance = 2;
+            System.out.println("Present: Full-time");
         }
     }
-    public void calculateDailyWage(){
+
+    public void calculateDailyWage() {
         if(dailyAttendance == 1){
+            dailyWage = wagePerHour * partTimeHour;
+            System.out.println("Daily Employee Wage: " + dailyWage);
+        } else if(dailyAttendance == 2){
             dailyWage = wagePerHour * fullDayHour;
             System.out.println("Daily Employee Wage: " + dailyWage);
-        }else{
+        } else {
             System.out.println("No pay");
         }
     }
