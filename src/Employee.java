295d32eeb62ad856651b773f97027265a1f2b048
angelina <angelinasennichan@gmail.com>
2025-12-10 12:41:21 +0530

    Added: Daily Employee Wage Calculation

--- src/Employee.java
+++ src/Employee.java
@@ -1,5 +1,9 @@
 public class Employee {
     int dailyAttendance;
+    int wagePerHour = 20;
+    int fullDayHour = 8;
+    int dailyWage;
+
     public void calculateAttendance(){
         if(Math.random()< 0.5){
             dailyAttendance = 0;
@@ -7,6 +11,14 @@ public class Employee {
         }else {
             dailyAttendance = 1;
             System.out.println("Present");
+        }
+    }
+    public void calculateDailyWage(){
+        if(dailyAttendance == 1){
+            dailyWage = wagePerHour * fullDayHour;
+            System.out.println("Daily Employee Wage: " + dailyWage);
+        }else{
+            System.out.println("No pay");
         }
     }
 }
